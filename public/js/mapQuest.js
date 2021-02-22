5f2ecb576d69574ac1400c7ab347953cb90bfa0d
AxiosDeminence <juhmer@att.net>
2021-02-22 01:42:39 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'NqxnYmgkvvGYc72gdxbCxtQqjp1UvucT';
+
+// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+	center: [32.87920508226757, -117.23614349207305],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12
+	});
 }
